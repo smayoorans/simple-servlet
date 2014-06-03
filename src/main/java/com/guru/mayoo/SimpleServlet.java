fce2f6f33da4cf39f217a8e76b321a739edaa2ff
Mayooran <mayooran@hsenidmobile.com>
2014-06-03 15:09:03 +0530

    Added simple servlet example

---
+++ src/main/java/com/guru/mayoo/SimpleServlet.java
@@ -0,0 +1,26 @@
+package com.guru.mayoo;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SimpleServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("SimpleServlet GET Request Executed");
+        out.flush();
+        out.close();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("SimpleServlet POST Request Executed");
+        out.flush();
+        out.close();
+    }
+}
